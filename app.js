1e3428e23f9c0734a7341970e6d5f42c9ed7d429
ysemenyuk <invent@list.ru>
2021-10-22 09:40:04 +0300

    cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var isProduction = process.env.NODE_ENV === 'production';
 // Create global app object
 var app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 // Normal express config defaults
 app.use(require('morgan')('dev'));
