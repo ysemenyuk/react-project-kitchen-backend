6cd21ec565e41dd86bad29a2118a5aa6df8df397
ysemenyuk <invent@list.ru>
2021-10-23 08:53:59 +0300

    cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var isProduction = process.env.NODE_ENV === 'production';
 // Create global app object
 var app = express();
 
-app.use(cors({ allowedHeaders: ['Content-Type', 'Authorization'] }));
+app.use(cors({ origin: /\.herokuapp\.com$/ }));
 
 // Normal express config defaults
 app.use(require('morgan')('dev'));
